a673b45f1209b499561866cb89b5a093b75f0636
Joel IV Sulit <joelivsulit@gmail.com>
2026-02-03 20:23:56 +0800

    Enhance guessing game with life counter and validation
    Refactor guessing logic to include life counter and improve input validation.

--- guess.py
+++ guess.py
@@ -4,18 +4,28 @@ print("Welcome to Guess the Number Game!")
 print()
 
 randomNumber = random.randint(1, 100)
+life = 100
 
-guessNumber = int(input("Guess the number between 1 - 100: "))
-
-while guessNumber != randomNumber:
-
-    if guessNumber < 1 or guessNumber > 100:
-        print("The number is not between 1 and 100.")
-    elif guessNumber > randomNumber:
-        print("Too High!")
-    else:
-        print("Too Low!")
-
+while life > 0:
     guessNumber = int(input("Guess the number between 1 - 100: "))
+    while guessNumber != randomNumber:
+        if guessNumber < 1 or guessNumber > 100:
+            print("The number is not between 1 and 100.")
+        elif guessNumber > randomNumber:
+            print("Too High!")
+        else:
+            print("Too Low!")
 
-print(f"The number {guessNumber} is correct, congratulations!")
+        life -= 1
+        print(f"You have {life} more guesses left.")
+
+        if life == 0:
+            print("You don't have any more chances!")
+            break
+
+        guessNumber = int(input("Guess the number between 1 - 100: "))
+
+
+    if randomNumber == guessNumber:
+        print(f"Congratulations! The number {guessNumber} is correct.")
+        break
